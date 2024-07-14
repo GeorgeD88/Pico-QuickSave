60c965181f97bd7d72d35805077113772bed110c
George Doujaiji <GeorgeD.88888@gmail.com>
2024-07-14 08:02:58 -0400

    Remove signal pause and rename undo_last to undo_save

--- src/raspi_listener.py
+++ src/raspi_listener.py
@@ -1,12 +1,10 @@
-# Raspberry Pi
 from picozero import Button
-from signal import pause  # from regular Pi, TODO: check if Pico uses the same
 
-# Constants
 from actions import TOGGLE_LIKE, SAVE_MAIN, SAVE_OTHER, UNDO_SAVE, QUIT_APP
 
 """ NOTE: Pulled from the OG QuickSave. I'm going to convert this to work with the Pico,
     as well as add functions or refactor as needed. """
+
 
 class RasPiListener:
 
@@ -15,20 +13,21 @@ class RasPiListener:
         self.callback = button_callback
 
         # Set GPIO pin numbers
-        self.toggle_like_button = Button(gpio_pins['button_save_lib'])
+        self.toggle_like_button = Button(gpio_pins['button_toggle_like'])
         self.save_main_button = Button(gpio_pins['button_save_main'])
         self.save_other_button = Button(gpio_pins['button_save_other'])
-        self.undo_last_button = Button(gpio_pins['button_undo_save'])
+        self.undo_save_button = Button(gpio_pins['button_undo_save'])
 
         # Map the buttons to the actions
         self.toggle_like_button.when_pressed = self.toggle_like
         self.save_main_button.when_pressed = self.save_main
         self.save_other_button.when_pressed = self.save_other
-        self.undo_last_button.when_pressed = self.undo_last
+        self.undo_save_button.when_pressed = self.undo_save
 
     def start_listener(self):
         """ Starts the listener by waiting for signals. """
         print('start saving! 🎧🥧\n')
+        # TODO: refactor to use interrupts, because micro python can't signal.pause
         pause()  # Tells the RasPi to wait for signals (from buttons)
 
     def stop_listener(self):
@@ -44,5 +43,5 @@ class RasPiListener:
     def save_other(self):
         self.callback(SAVE_OTHER)
 
-    def undo_last(self):
+    def undo_save(self):
         self.callback(UNDO_SAVE)
