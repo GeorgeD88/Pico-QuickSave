65887aa50e2f4f5cbed574355cd6665242b23838
George Doujaiji <GeorgeD.88888@gmail.com>
2024-07-17 19:02:26 -0400

    Refactor the listener to use interrupts

--- src/raspi_listener.py
+++ src/raspi_listener.py
@@ -1,4 +1,5 @@
 from picozero import Button
+from machine import Pin
 
 from actions import TOGGLE_LIKE, SAVE_MAIN, SAVE_OTHER, UNDO_SAVE, QUIT_APP
 
@@ -12,36 +13,36 @@ class RasPiListener:
 
         self.callback = button_callback
 
-        # Set GPIO pin numbers
-        self.toggle_like_button = Button(gpio_pins['button_toggle_like'])
-        self.save_main_button = Button(gpio_pins['button_save_main'])
-        self.save_other_button = Button(gpio_pins['button_save_other'])
-        self.undo_save_button = Button(gpio_pins['button_undo_save'])
+        # Initialize GPIO pins
+        self.toggle_like_button = Pin(gpio_pins['button_toggle_like'], Pin.IN, Pin.PULL_UP)
+        self.save_main_button = Pin(gpio_pins['button_save_main'], Pin.IN, Pin.PULL_UP)
+        self.save_other_button = Pin(gpio_pins['button_save_other'], Pin.IN, Pin.PULL_UP)
+        self.undo_save_button = Pin(gpio_pins['button_undo_save'], Pin.IN, Pin.PULL_UP)
 
-        # Map the buttons to the actions
-        self.toggle_like_button.when_pressed = self.toggle_like
-        self.save_main_button.when_pressed = self.save_main
-        self.save_other_button.when_pressed = self.save_other
-        self.undo_save_button.when_pressed = self.undo_save
+        # Set up the buttons' triggers and handlers (callback)
+        self.toggle_like_button.irq(trigger=Pin.IRQ_FALLING, handler=self.toggle_like)
+        self.save_main_button.irq(trigger=Pin.IRQ_FALLING, handler=self.save_main)
+        self.save_other_button.irq(trigger=Pin.IRQ_FALLING, handler=self.save_other)
+        self.undo_save_button.irq(trigger=Pin.IRQ_FALLING, handler=self.undo_save)
+        # TODO: wiggling buttons triggers multiple times. maybe add bounce back or something?
 
     def start_listener(self):
         """ Starts the listener by waiting for signals. """
         print('start saving! 🎧🥧\n')
         # TODO: refactor to use interrupts, because micro python can't signal.pause
-        pause()  # Tells the RasPi to wait for signals (from buttons)
 
     def stop_listener(self):
         """ Placeholder because the parent component has to call this function to work. """
         print('thanks for stopping by! hopefully I was helpful :)')
 
-    def toggle_like(self):
+    def toggle_like(self, pin):
         self.callback(TOGGLE_LIKE)
 
-    def save_main(self):
+    def save_main(self, pin):
         self.callback(SAVE_MAIN)
 
-    def save_other(self):
+    def save_other(self, pin):
         self.callback(SAVE_OTHER)
 
-    def undo_save(self):
+    def undo_save(self, pin):
         self.callback(UNDO_SAVE)
