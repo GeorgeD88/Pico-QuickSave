8ca8a7f5de192ff2ce7e4fe26bf8648ae495b9b4
George Doujaiji <GeorgeD.88888@gmail.com>
2024-07-18 07:20:25 -0400

    Add Button class with debouncing logic
    We implemented button interrupts in the last commit, but this time we
    refactored it to add debouncing logic. In doing that, we created a new
    Button class that encapsulates all the button pin code and debouncing.
    We also removed the start and stop listener methods because they are not
    needed.

--- src/raspi_listener.py
+++ src/raspi_listener.py
@@ -1,48 +1,54 @@
-from picozero import Button
 from machine import Pin
+import time
 
 from actions import TOGGLE_LIKE, SAVE_MAIN, SAVE_OTHER, UNDO_SAVE, QUIT_APP
 
-""" NOTE: Pulled from the OG QuickSave. I'm going to convert this to work with the Pico,
-    as well as add functions or refactor as needed. """
+
+class Button:
+
+    def __init__(self, pin: int, callback):
+        # Define the callback and debounce variables
+        self.callback = callback
+        self.debounce_time = 300  # 300 milliseconds debounce time
+        self.last_press_time = 0
+
+        # Set up the button pin and interrupt request (IRQ)
+        self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
+        self.pin.irq(trigger=Pin.IRQ_FALLING, handler=self.handle_interrupt)
+
+    def is_pressed(self):
+        """ Checks if the button press is valid based on debounce time. """
+        current_time = time.ticks_ms()
+        if time.ticks_diff(current_time, self.last_press_time) > self.debounce_time:
+            self.last_press_time = current_time
+            return True
+        return False
+
+    def handle_interrupt(self, pin):
+        """ Triggers the callback only if the button press is valid based on debounce time. """
+        if self.is_pressed():
+            self.callback()
 
 
 class RasPiListener:
 
     def __init__(self, button_callback, gpio_pins: dict):
-
         self.callback = button_callback
 
-        # Initialize GPIO pins
-        self.toggle_like_button = Pin(gpio_pins['button_toggle_like'], Pin.IN, Pin.PULL_UP)
-        self.save_main_button = Pin(gpio_pins['button_save_main'], Pin.IN, Pin.PULL_UP)
-        self.save_other_button = Pin(gpio_pins['button_save_other'], Pin.IN, Pin.PULL_UP)
-        self.undo_save_button = Pin(gpio_pins['button_undo_save'], Pin.IN, Pin.PULL_UP)
+        # Initialize buttons and map actions
+        self.toggle_like_button = Button(gpio_pins['button_toggle_like'], self.toggle_like)
+        self.save_main_button = Button(gpio_pins['button_save_main'], self.save_main)
+        self.save_other_button = Button(gpio_pins['button_save_other'], self.save_other)
+        self.undo_save_button = Button(gpio_pins['button_undo_save'], self.undo_save)
 
-        # Set up the buttons' triggers and handlers (callback)
-        self.toggle_like_button.irq(trigger=Pin.IRQ_FALLING, handler=self.toggle_like)
-        self.save_main_button.irq(trigger=Pin.IRQ_FALLING, handler=self.save_main)
-        self.save_other_button.irq(trigger=Pin.IRQ_FALLING, handler=self.save_other)
-        self.undo_save_button.irq(trigger=Pin.IRQ_FALLING, handler=self.undo_save)
-        # TODO: wiggling buttons triggers multiple times. maybe add bounce back or something?
-
-    def start_listener(self):
-        """ Starts the listener by waiting for signals. """
-        print('start saving! 🎧🥧\n')
-        # TODO: refactor to use interrupts, because micro python can't signal.pause
-
-    def stop_listener(self):
-        """ Placeholder because the parent component has to call this function to work. """
-        print('thanks for stopping by! hopefully I was helpful :)')
-
-    def toggle_like(self, pin):
+    def toggle_like(self):
         self.callback(TOGGLE_LIKE)
 
-    def save_main(self, pin):
+    def save_main(self):
         self.callback(SAVE_MAIN)
 
-    def save_other(self, pin):
+    def save_other(self):
         self.callback(SAVE_OTHER)
 
-    def undo_save(self, pin):
+    def undo_save(self):
         self.callback(UNDO_SAVE)
