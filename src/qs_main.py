b997e0ff492d0a2214d5f27849e531f651868680
George Doujaiji <GeorgeD.88888@gmail.com>
2024-07-19 10:39:00 -0400

    Add auto wifi connect, set rtc time, and clean up

--- src/qs_main.py
+++ src/qs_main.py
@@ -1,54 +1,28 @@
+from web_connect import try_wifi_networks
 from quicksaver import QuickSaver
-# TODO: remove 'raspi' prefix from files because all files are for RasPi
-#from raspi_listener import RasPiListener
-import json
-
-
-# TODO: finalize this but I think I should maybe put it in a different file.
-# I should also use the 15 line function rule (from SWE class), and that will look better in a different file.
-def connect_wlan():
-    """
-    if self.config['setup_network']:
-        self.wlan_ap = network.WLAN(network.AP_IF)
-        self.wlan_ap.active(False)
-        self.wlan = network.WLAN(network.STA_IF)
-        try:
-            self.wlan.active(True)
-            self.wlan.connect(self.config['wlan']['ssid'], self.config['wlan']['password'])
-            self.wlan.config(dhcp_hostname=self.config['wlan']['mdns'])
-        except Exception as e:
-            self.oled.show(e.__class__.__name__, str(e))
-            if str(e) == "Wifi Internal Error":
-                time.sleep(3)
-                import machine
-                machine.reset()
-            else:
-                raise
-        print("network configured")
-    else:
-        self.wlan = network.WLAN()
-        print("using existing network configuration")
-
-    self.ip = self.wlan.ifconfig()[0]
-            print("connected at {} as {}".format(self.ip, self.config['wlan']['mdns']))
-    """
-    pass
+from rtc_time import set_time
+import ujson
+import errno
 
 
 def main():
-    # Load config file and get playlist IDs
-    with open('config.json', 'r') as infile:
-        config = json.load(infile)
+    # Load config
+    try:
+        config = config.get_config()
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            print(f'[{errno.errorcode[e.errno]}]', 'Config file does not exist')
 
-    # TODO: consider checking if playlist IDs exist on Spotify
-    # if not, create new ones and save the IDs to config file.
-    main_playlist = config['playlists']['main_playlist']
-    other_playlist = config['playlists']['other_playlist']
+    # Connect to Wi-Fi network
+    network_details = config['wlan']
+    connected_ssid = try_wifi_networks(network_details)
 
-    # initialize the main quick saver component and start the input listener
-    print('starting QuickSaver!')
+    # After Pico is connected to Wi-Fi, set the current time
+    set_time()
+
+    # Initialize the main QuickSaver component and start running QuickSaver
     quicksaver = QuickSaver(ConsoleListener, SystemNotifier, main_playlist, other_playlist)
-    quicksaver.start_quicksaver()  # renamed from `start_input_listener`
+    quicksaver.start_quicksaver()
 
 
 if __name__ == "__main__":
